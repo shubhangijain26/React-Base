968a0823b36bbae18508fac9199a65d499e2d1d0
Shubhangi <shubhangijain26@yahoo.com>
2026-06-24 17:04:19 +0530

    Episode 4

---
+++ mocks/data.js
@@ -0,0 +1,335 @@
+export const RESTAURANT_DATA = [
+  {
+    id: "660101",
+    name: "The Square - Novotel",
+    rating: "4.1",
+    costForTwo: "₹2500 for two",
+    cuisines: ["Multi Cuisine", "North Indian"],
+    locationInfo: {
+      distanceString: "0.5 km",
+      landmarkName: "Novotel Bengaluru Outer Ring Road",
+      city: { name: "Bangalore" },
+      formattedAddress:
+        "Novotel Bengaluru Outer Ring Road, Marathahalli, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2024-01-29T10:28:08.407",
+        url: "v1706524090/9d101264c7c4863159054d663d1fe8c5.jpg",
+      },
+      {
+        type: "IMAGE",
+        id: "2022-12-27T20:30:32.967",
+        name: "ambience",
+        url: "v1672173035/2b8048193551be829840a67c416610f1.jpg",
+      },
+    ],
+  },
+  {
+    id: "278555",
+    name: "Momo Cafe - Courtyard by Marriott",
+    rating: "4.0",
+    costForTwo: "₹3000 for two",
+    cuisines: ["Chinese", "Fast Food"],
+    locationInfo: {
+      distanceString: "1.4 km",
+      landmarkName: "Courtyard by Marriott Bengaluru Outer Ring Road",
+      city: { name: "Bangalore" },
+      formattedAddress:
+        "Courtyard by Marriott Bengaluru Outer Ring Road, Marathahalli, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2022-08-22T21:30:02.557",
+        name: "ambience",
+        url: "v1672173116/88c1500889e2b28056118ebd83b3d5e6.jpg",
+      },
+      {
+        type: "IMAGE",
+        id: "2022-08-22T21:30:22.787",
+        name: "ambience",
+        url: "v1661203825/ujkqhbd8jr5mddb2ql5z.jpg",
+      },
+    ],
+  },
+  {
+    id: "662114",
+    name: "Blue Terrain - Novotel",
+    rating: "4.5",
+    costForTwo: "₹2000 for two",
+    cuisines: ["Multi Cuisine", "Continental"],
+    locationInfo: {
+      distanceString: "1.1 km",
+      landmarkName: "Novotel Bengaluru Outer Ring Road",
+      city: { name: "Bangalore" },
+      formattedAddress:
+        "Novotel Bengaluru Outer Ring Road, Marathahalli, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2024-01-29T10:31:18.562",
+        url: "v1706524280/99d06e2d17746eb5a57f828ad2df9314.jpg",
+      },
+      {
+        type: "IMAGE",
+        id: "2024-01-29T10:31:19.489",
+        url: "v1706524281/1f012c0ab9d87fe0402dd5e54e02dd7b.jpg",
+      },
+    ],
+  },
+  {
+    id: "660133",
+    name: "Chez Nous - Novotel",
+    rating: "4.4",
+    costForTwo: "₹2500 for two",
+    cuisines: ["Continental", "European"],
+    locationInfo: {
+      distanceString: "0.5 km",
+      landmarkName: "Novotel Bengaluru Outer Ring Road",
+      city: { name: "Bangalore" },
+      formattedAddress:
+        "Novotel Bengaluru Outer Ring Road, Marathahalli, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2024-01-29T10:33:59.243",
+        url: "v1672834412/983b777b6d5c99aedf2b70883b812653.webp",
+      },
+      {
+        type: "IMAGE",
+        id: "2024-01-29T10:33:59.024",
+        url: "v1706524440/1b428c11c66d2780bceabfea18f7d5a8.jpg",
+      },
+    ],
+  },
+  {
+    id: "949354",
+    name: "Swad-E-Punjab",
+    rating: "4.3",
+    costForTwo: "₹1200 for two",
+    cuisines: ["North Indian"],
+    locationInfo: {
+      distanceString: "2.1 km",
+      city: { name: "Bangalore" },
+      formattedAddress: "Bellandur, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2023-08-01T05:05:58.245",
+        name: "Dish",
+        url: "v1690866361/d8ded5713979c648372eebf645e24220.jpg",
+      },
+    ],
+  },
+  {
+    id: "34134",
+    name: "Sichuan",
+    rating: "4.3",
+    costForTwo: "₹800 for two",
+    cuisines: ["Chinese", "Sichuan"],
+    locationInfo: {
+      distanceString: "1.6 km",
+      city: { name: "Bangalore" },
+      formattedAddress: "Bellandur, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2022-09-02T21:22:53.150",
+        name: "ambience",
+        url: "v1662153852/slcgcw1t3ervreyadekx.jpg",
+      },
+      {
+        type: "IMAGE",
+        id: "2022-09-02T21:22:56.719",
+        name: "ambience",
+        url: "v1662153779/xdgtkdtd6wmioeoqziwv.jpg",
+      },
+    ],
+  },
+  {
+    id: "1353805",
+    name: "Chalo Punjab",
+    rating: "4.5",
+    costForTwo: "₹1500 for two",
+    cuisines: ["North Indian"],
+    locationInfo: {
+      distanceString: "1 km",
+      city: { name: "Bangalore" },
+      formattedAddress: "bellandur, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2023-01-04T09:33:38.522",
+        url: "v1672824819/4c7355802dd043e89aa17296bcaa4d88.webp",
+      },
+    ],
+  },
+  {
+    id: "871879",
+    name: "Biergarten",
+    rating: "4.6",
+    costForTwo: "₹2500 for two",
+    cuisines: ["Bar Food", "Continental"],
+    locationInfo: {
+      distanceString: "3.8 km",
+      city: { name: "Bangalore" },
+      formattedAddress: "Bellandur, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2023-08-01T05:05:58.245",
+        name: "Dish",
+        url: "v1662153856/uyxczb9usgaqlga7hhfl.jpg",
+      },
+    ],
+  },
+  {
+    id: "178368",
+    name: "Spice It - IBIS Hotel ORR",
+    rating: "4.2",
+    costForTwo: "₹1500 for two",
+    cuisines: ["Multi Cuisine", "Continental"],
+    locationInfo: {
+      distanceString: "1.1 km",
+      landmarkName: "IBIS Hotel - Outer Ring Road",
+      city: { name: "Bangalore" },
+      formattedAddress: "IBIS Hotel - Outer Ring Road, Bellandur, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2023-01-04T09:33:57.821",
+        url: "v1672824839/0341d5a1f9d152fa751a44ca80b5452e.webp",
+      },
+      {
+        type: "IMAGE",
+        id: "2023-01-04T09:33:57.841",
+        url: "v1672824839/853a46e00f7aba39c813b184e5d2e51c.webp",
+      },
+    ],
+  },
+  {
+    id: "1368296",
+    name: "Smash guys - burger kitchen",
+    rating: "4.5",
+    costForTwo: "₹1500 for two",
+    cuisines: ["American", "Continental"],
+    locationInfo: {
+      distanceString: "2.6 km",
+      city: { name: "Bangalore" },
+      formattedAddress: "Bellandur, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2022-12-27T20:30:32.967",
+        name: "ambience",
+        url: "v1672173035/2b8048193551be829840a67c416610f1.jpg",
+      },
+    ],
+  },
+  {
+    id: "747474",
+    name: "Kava - Fairfield by Marriott",
+    rating: "3.6",
+    costForTwo: "₹1600 for two",
+    cuisines: ["Multi Cuisine", "North Indian"],
+    locationInfo: {
+      distanceString: "1.4 km",
+      landmarkName: "Fairfield By Marriott - Outer Ring Road",
+      city: { name: "Bangalore" },
+      formattedAddress:
+        "Fairfield By Marriott - Outer Ring Road, Bellandur, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2023-08-01T05:05:50.761",
+        name: "Dish",
+        url: "v1690866353/172889a046d19165aada8d06dfb10ca8.jpg",
+      },
+      {
+        type: "IMAGE",
+        id: "2023-08-01T05:05:58.245",
+        name: "Dish",
+        url: "v1690866361/d8ded5713979c648372eebf645e24220.jpg",
+      },
+    ],
+  },
+  {
+    id: "328612",
+    name: "Publik",
+    rating: "4.3",
+    costForTwo: "₹1400 for two",
+    cuisines: ["Bar Food", "Continental"],
+    locationInfo: {
+      distanceString: "3.8 km",
+      landmarkName: "Icon Premier Hotel",
+      city: { name: "Bangalore" },
+      formattedAddress: "Icon Premier Hotel, Bellandur, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2022-08-23T23:06:43.893",
+        name: "ambience",
+        url: "v1661296006/ntv1i9jbrgre5bm8ppfb.jpg",
+      },
+      {
+        type: "IMAGE",
+        id: "2022-08-23T23:06:35.651",
+        name: "ambience",
+        url: "v1661295998/epaar3l8g869mfd2p3qe.jpg",
+      },
+    ],
+  },
+  {
+    id: "1364685",
+    name: "TATSA The Indian Kitchen",
+    rating: "4.8",
+    costForTwo: "₹1000 for two",
+    cuisines: ["North Indian", "Indian Cuisine"],
+    locationInfo: {
+      distanceString: "0.9 km",
+      city: { name: "Bangalore" },
+      formattedAddress: "Devarabisanahalli, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2022-08-22T21:31:08.434",
+        name: "Dish",
+        url: "v1661203870/hhhq4mzvmlab51onl8r1.jpg",
+      },
+    ],
+  },
+  {
+    id: "981366",
+    name: "Lucky Chan - Dimsum & Sushi Parlour",
+    rating: "4.6",
+    costForTwo: "₹1000 for two",
+    cuisines: ["Asian", "Sushi"],
+    locationInfo: {
+      distanceString: "2.6 km",
+      landmarkName: "The Bay at Ecoworld",
+      city: { name: "Bangalore" },
+      formattedAddress: "The Bay at Ecoworld, Bellandur, Bangalore",
+    },
+    mediaFiles: [
+      {
+        type: "IMAGE",
+        id: "2022-12-27T21:07:57.686",
+        name: "Dish",
+        url: "v1672175279/e56a9e0e4d8e563a9e0791c1b6309d1f.jpg",
+      },
+    ],
+  },
+];
